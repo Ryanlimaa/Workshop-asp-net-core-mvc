7ef1b9d97d97b60842c479e2926178869e4eb269
ryan.limaa@outlook.com <ryan.limaa@outlook.com>
2026-02-18 16:20:23 -0300

    Primeiro MVC Departamento

---
+++ ProjetoWebMVC/Controllers/DepartamentosController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoWebMVC.Models;
+using System.Collections.Generic;
+
+namespace ProjetoWebMVC.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> list = new List<Departamento>();
+            list.Add(new Departamento { Id = 1, Name = "Eletronicos" });
+            list.Add(new Departamento { Id = 2, Name = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
---
+++ ProjetoWebMVC/Models/Departamento.cs
@@ -0,0 +1,8 @@
+﻿ namespace ProjetoWebMVC.Models
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
