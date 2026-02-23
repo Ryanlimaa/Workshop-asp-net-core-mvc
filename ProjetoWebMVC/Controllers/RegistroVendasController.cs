6523fe7e226f96629da433d6a30c71c5fa21b629
ryan.limaa@outlook.com <ryan.limaa@outlook.com>
2026-02-23 13:30:43 -0300

    Preparando a navegacao das paginas de busca

---
+++ ProjetoWebMVC/Controllers/RegistroVendasController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoWebMVC.Controllers
+{
+    public class RegistroVendasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaSimples()
+        {
+            return View();
+        }
+
+        public IActionResult BuscaAgrupada()
+        {
+            return View();
+        }
+    }
+}
