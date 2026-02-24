b08b7159587153a168c79dd7d388cb9cf3a3f83b
ryan.limaa@outlook.com <ryan.limaa@outlook.com>
2026-02-24 15:41:31 -0300

    Implementando a busca agrupada

--- ProjetoWebMVC/Models/RegistroVenda.cs
+++ ProjetoWebMVC/Models/RegistroVenda.cs
@@ -1,12 +1,19 @@
 ﻿using ProjetoWebMVC.Models.Enums;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjetoWebMVC.Models
 {
     public class RegistroVenda
     {
         public int Id { get; set; }
+
+        [Display(Name = "Data")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [Display(Name = "Valor")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; } // Valor da venda    
         public StatusVendedor Status { get; set; }
         public Vendedor Vendedor { get; set; }
--- ProjetoWebMVC/Services/RegistroVendaService.cs
+++ ProjetoWebMVC/Services/RegistroVendaService.cs
@@ -34,5 +34,24 @@ namespace ProjetoWebMVC.Services
                 .OrderByDescending(x => x.Date)
                 .ToListAsync();
         }
+
+        public async Task<List<IGrouping<Departamento,RegistroVenda>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.RegistroVenda select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Vendedor)
+                .Include(x => x.Vendedor.Departamento)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Vendedor.Departamento)
+                .ToListAsync();
+        }
     }
 }
